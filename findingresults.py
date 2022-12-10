b2111dbcc71cc70f57565b0e1dc37b230b10c503
gopancakes <samuelfdove@gmail.com>
2022-12-10 17:19:03 -0600

    adding some analysis

--- findingresults.py
+++ findingresults.py
@@ -21,11 +21,22 @@ db = sqlite3.connect("gameheaders.db")
 # df = pd.read_sql_query("select tosquarerank,tosquarefile,piece,COUNT(*) from allmoves group by tosquarerank,tosquarefile,piece",db)
 # df.to_csv('results/piece_to_square_counts.csv',index=False)
 
-#PIECE FROM SQUARE COUNTS IGNORE BOOK
-df = pd.read_sql_query("select fromsquarerank,fromsquarefile,piece,COUNT(*) from allmoves where isbook=0 group by fromsquarerank,fromsquarefile,piece",db)
-df.to_csv('results/piece_from_square_counts_ignore_book.csv',index=False)
+# #PIECE FROM SQUARE COUNTS IGNORE FIRST 10
+# df = pd.read_sql_query("select fromsquarerank,fromsquarefile,piece,COUNT(*) from allmoves where isbook=0 and ply > 10 group by fromsquarerank,fromsquarefile,piece",db)
+# df.to_csv('results/piece_from_square_counts_ignore_10.csv',index=False)
 
-#PIECE TO SQUARE COUNTS IGNORE BOOK
-df = pd.read_sql_query("select tosquarerank,tosquarefile,piece,COUNT(*) from allmoves where isbook=0 group by tosquarerank,tosquarefile,piece",db)
-df.to_csv('results/piece_to_square_counts_ignore_book.csv',index=False)
+# #PIECE TO SQUARE COUNTS IGNORE FIRST 10
+# df = pd.read_sql_query("select tosquarerank,tosquarefile,piece,COUNT(*) from allmoves where isbook=0 and ply > 10 group by tosquarerank,tosquarefile,piece",db)
+# df.to_csv('results/piece_to_square_counts_ignore_10.csv',index=False)
 
+# #PIECE FROM SQUARE COUNTS BY SIDE
+# df = pd.read_sql_query("select fromsquarerank,fromsquarefile,piece,COUNT(*),turn from allmoves where isbook=0 group by fromsquarerank,fromsquarefile,piece,turn",db)
+# df.to_csv('results/piece_from_square_counts_by_side.csv',index=False)
+
+# #PIECE TO SQUARE COUNTS IGNORE SIDE
+# df = pd.read_sql_query("select tosquarerank,tosquarefile,piece,COUNT(*),turn from allmoves where isbook=0 group by tosquarerank,tosquarefile,piece,turn",db)
+# df.to_csv('results/piece_to_square_counts_by_side.csv',index=False)
+
+# #PIECE DISTANCE BY ELO --Didn't give much
+# df = pd.read_sql_query("SELECT piece, avgrating, avg(distance) from allmoves join gameExtraData using (tell) group by piece, avgrating", db)
+# df.to_csv('results/piece_distance_by_rating.csv', index=False)
